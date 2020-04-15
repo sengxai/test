5a5b925a17fb3f80dfb4b3f24aa015e470ccf345
Seng Xai Yang <yangsx7908@uwec.edu>
2020-04-14 22:40:00 -0500

    test

---
+++ precache-manifest.f67d2164b88fd573917492fbab51c5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2aa53cd92b942fe48cb7d3ba76a2fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fdf4b2230ea8f77dba",
+    "url": "/static/css/main.cc1b079a.chunk.css"
+  },
+  {
+    "revision": "2728a0d0637f81ea08ea",
+    "url": "/static/js/2.ba528d29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba528d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fdf4b2230ea8f77dba",
+    "url": "/static/js/main.5ce76932.chunk.js"
+  },
+  {
+    "revision": "223152c33a0d2769db47",
+    "url": "/static/js/runtime-main.dbab5227.js"
+  }
+]);
